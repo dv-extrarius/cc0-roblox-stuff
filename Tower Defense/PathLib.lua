c80905da0107098e3afc4da70b94101a92927b96
dv-extrarius <12847508+dv-extrarius@users.noreply.github.com>
2024-01-13 16:21:55 -0600

    Expanding PathLib API
    Updated API to allow finding multiple paths to the goal without having to recompute the node costs for each path.

--- Tower Defense/PathLib.lua
+++ Tower Defense/PathLib.lua
@@ -44,41 +44,64 @@ local GRID_INDEX_MUL = 131072
 local function CoordToIndex(x: number, z: number): number
     return (x + (GRID_INDEX_MUL/2)) + ((z + (GRID_INDEX_MUL/2)) * GRID_INDEX_MUL)
 end
+
+
 --Convert an adjustment to a coordinate to an adjustment to an index
 local function DeltaCoordToIndex(x: number, z: number): number
     return (x) + (z * GRID_INDEX_MUL)
 end
 
 
---Functions to move coordinates to pathing grid
-function PathLib.ToPathGridRound(coord: Vector3): Vector3
+--Function to snap coordinates to pathing grid by rounding
+local function ToPathGridRound(coord: Vector3): Vector3
     return Vector3.new(
         math.round((coord.X - GRID_COORD_OFFSET) / GRID_COORD_SPACING) * GRID_COORD_SPACING + GRID_COORD_OFFSET,
         0,
         math.round((coord.Z - GRID_COORD_OFFSET) / GRID_COORD_SPACING) * GRID_COORD_SPACING + GRID_COORD_OFFSET
     )
 end
+PathLib.ToPathGridRound = ToPathGridRound
 
-function PathLib.ToPathGridFloor(coord: Vector3): Vector3
+
+--Function to snap coordinates to pathing grid by flooring
+function ToPathGridFloor(coord: Vector3): Vector3
     return Vector3.new(
         math.floor((coord.X - GRID_COORD_OFFSET) / GRID_COORD_SPACING) * GRID_COORD_SPACING + GRID_COORD_OFFSET,
         0,
         math.floor((coord.Z - GRID_COORD_OFFSET) / GRID_COORD_SPACING) * GRID_COORD_SPACING + GRID_COORD_OFFSET
     )
 end
+PathLib.ToPathGridFloor = ToPathGridFloor
 
-function PathLib.ToPathGridCeil(coord: Vector3): Vector3
+
+--Function to snap coordinates to pathing grid by ceiling
+function ToPathGridCeil(coord: Vector3): Vector3
     return Vector3.new(
         math.ceil((coord.X - GRID_COORD_OFFSET) / GRID_COORD_SPACING) * GRID_COORD_SPACING + GRID_COORD_OFFSET,
         0,
         math.ceil((coord.Z - GRID_COORD_OFFSET) / GRID_COORD_SPACING) * GRID_COORD_SPACING + GRID_COORD_OFFSET
     )
 end
+PathLib.ToPathGridCeil = ToPathGridCeil
+
+
+--Function to fit an area to the grid
+local function AreaToGrid(min: Vector3, max: Vector3, includePartial: boolean?): (Vector3, Vector3)
+    local high = Vector3.new(math.max(max.X, min.X), math.max(max.Y, min.Y), math.max(max.Z, min.Z))
+    local low = Vector3.new(math.min(max.X, min.X), math.min(max.Y, min.Y), math.min(max.Z, min.Z))
+    if not includePartial then
+        return ToPathGridCeil(low), ToPathGridFloor(high)
+    else
+        return ToPathGridFloor(low), ToPathGridCeil(high)
+    end
+end
+
 
 --Create an empty mesh
 function PathLib.NewMesh(): PathMesh
     return {Nodes = {}, IsFinalized = false}
 end
+
 
 --Clone a mesh. Finalizes the nodes if the original is finialized
 function PathLib.Clone(mesh: PathMesh): PathMesh
@@ -86,7 +109,7 @@ function PathLib.Clone(mesh: PathMesh): PathMesh
 
     for index, current in mesh.Nodes do
         local node = table.clone(current)
-        node[IDX_NEIGHBORS] = {}
+        node[IDX_NEIGHBORS] = table.create(4)
     end
 
     if mesh.IsFinalized then
@@ -95,17 +118,12 @@ function PathLib.Clone(mesh: PathMesh): PathMesh
     return newMesh
 end
 
+
 --Add nodes to a mesh to cover a specified region
-function PathLib.AddNodes(mesh: PathMesh, min: Vector3, max: Vector3, expand: boolean?): ()
+function PathLib.AddNodes(mesh: PathMesh, min: Vector3, max: Vector3, includePartial: boolean?): ()
     local nodes = mesh.Nodes
 
-    if not expand then
-        min = PathLib.ToPathGridCeil(min)
-        max = PathLib.ToPathGridFloor(max)
-    else
-        min = PathLib.ToPathGridFloor(min)
-        max = PathLib.ToPathGridCeil(max)
-    end
+    min, max = AreaToGrid(min, max, includePartial)
 
     for z = min.Z, max.Z, GRID_COORD_SPACING do
         local index = CoordToIndex(min.X, z)
@@ -113,12 +131,12 @@ function PathLib.AddNodes(mesh: PathMesh, min: Vector3, max: Vector3, expand: bo
         for x = min.X, max.X, GRID_COORD_SPACING do
             if nodes[index] == nil then 
                 nodes[index] = {
-                    {},                      --Neighbors
-                    Vector3.new(x, 0, z),    --Position 
-                    index,                   --Index
-                    math.random() / 10000.0, --RandomWeight 
-                    math.huge,               --TotalCost
-                    false                    --Blocked
+                    table.create(4),            --Neighbors
+                    Vector3.new(x, 0, z),       --Position 
+                    index,                      --Index
+                    (math.random() / 100000.0), --RandomWeight 
+                    math.huge,                  --TotalCost
+                    false                       --Blocked
                 }
             end
             index += DeltaCoordToIndex(GRID_COORD_SPACING, 0)
@@ -126,7 +144,8 @@ function PathLib.AddNodes(mesh: PathMesh, min: Vector3, max: Vector3, expand: bo
     end
 end
 
---Do final processing on each node in the mesh and freeze it
+
+--Do final processing on each node in the mesh
 function PathLib.FinalizeMesh(mesh: PathMesh): ()
     local nodes = mesh.Nodes
     local neighbor: PathNode?
@@ -157,20 +176,15 @@ function PathLib.FinalizeMesh(mesh: PathMesh): ()
     end
 
     mesh.IsFinalized = true
-    --table.freeze(mesh.Nodes)
+    table.freeze(mesh.Nodes)
 end
 
+
 --Mark all nodes in the specified region as blocked
-function PathLib.BlockNodes(mesh: PathMesh, min: Vector3, max: Vector3, expand: boolean?): ()
+function PathLib.BlockNodes(mesh: PathMesh, min: Vector3, max: Vector3, includePartial: boolean?): ()
     local nodes = mesh.Nodes
 
-    if not expand then
-        min = PathLib.ToPathGridCeil(min)
-        max = PathLib.ToPathGridFloor(max)
-    else
-        min = PathLib.ToPathGridFloor(min)
-        max = PathLib.ToPathGridCeil(max)
-    end
+    min, max = AreaToGrid(min, max, includePartial)
 
     for z = min.Z, max.Z, GRID_COORD_SPACING do
         local index = CoordToIndex(min.X, z)
@@ -186,17 +200,12 @@ function PathLib.BlockNodes(mesh: PathMesh, min: Vector3, max: Vector3, expand: 
     end
 end
 
+
 --Mark all nodes in the specified region as unblocked
-function PathLib.UnblockNodes(mesh: PathMesh, min: Vector3, max: Vector3, expand: boolean?): ()
+function PathLib.UnblockNodes(mesh: PathMesh, min: Vector3, max: Vector3, includePartial: boolean?): ()
     local nodes = mesh.Nodes
 
-    if not expand then
-        min = PathLib.ToPathGridCeil(min)
-        max = PathLib.ToPathGridFloor(max)
-    else
-        min = PathLib.ToPathGridFloor(min)
-        max = PathLib.ToPathGridCeil(max)
-    end
+    min, max = AreaToGrid(min, max, includePartial)
 
     for z = min.Z, max.Z, GRID_COORD_SPACING do
         local index = CoordToIndex(min.X, z)
@@ -212,28 +221,25 @@ function PathLib.UnblockNodes(mesh: PathMesh, min: Vector3, max: Vector3, expand
     end
 end
 
---Find a path between the given locations using the path mesh
-function PathLib.FindPath(mesh: PathMesh, start: Vector3, finish: Vector3): PathList?
-    --Look up the start and finish locations in the node grid
-    start = PathLib.ToPathGridRound(start)
-    finish = PathLib.ToPathGridRound(finish)
 
-    local startNode = mesh.Nodes[CoordToIndex(start.X, start.Z)]
-    local finishNode = mesh.Nodes[CoordToIndex(finish.X, finish.Z)]
-
-    if startNode == nil then
-        warn("Start point of (", start, ") is outside path nodes")
-        return nil
-    elseif startNode[IDX_BLOCKED] then
-        warn("Start node at (", start, ") is blocked!")
-        return nil
+--Utility to print warnings for invalid start/finish locations
+local function ValidateNode(label: string, location: Vector3, node: PathNode): boolean
+    if node == nil then
+        warn(label, " point of (", location, ") is outside path nodes")
+        return false
+    elseif node[IDX_BLOCKED] then
+        warn(label, " node at (", location, ") is blocked!")
+        return false
     end
-    if finishNode == nil then
-        warn("Finish point of (", finish, ") is outside path nodes")
-        return nil
-    elseif finishNode[IDX_BLOCKED] then
-        warn("Start node at (", finish, ") is blocked!")
-        return nil
+    return true
+end
+
+
+--Update the costs so paths can be found from anywhere to finishNode
+local function UpdateMeshCosts(mesh: PathMesh, finishNode: PathNode): boolean
+    --Finding a path requires a finalized mesh
+    if not mesh.IsFinalized then
+        return false
     end
 
     --Reset costs to maximum value
@@ -245,12 +251,12 @@ function PathLib.FindPath(mesh: PathMesh, start: Vector3, finish: Vector3): Path
     local frontWave: {[number]: PathNode} = {}
     local nextWave: {[number]: PathNode} = {}
 
-    frontWave[startNode[IDX_INDEX]] = startNode
-    startNode[IDX_TOTALCOST] = 0
-
+    frontWave[finishNode[IDX_INDEX]] = finishNode
+    finishNode[IDX_TOTALCOST] = 0
+    
     while next(frontWave) ~= nil do
-        for _, current in frontWave do
-            local TotalCost = current[IDX_TOTALCOST] + 1 + current[IDX_RANDOMWEIGHT]
+        for index, current in frontWave do
+            local TotalCost = current[IDX_TOTALCOST] + 1  + current[IDX_RANDOMWEIGHT]
 
             for _, neighbor in current[IDX_NEIGHBORS] do
                 if (not neighbor[IDX_BLOCKED]) and (TotalCost < neighbor[IDX_TOTALCOST]) then
@@ -262,26 +268,49 @@ function PathLib.FindPath(mesh: PathMesh, start: Vector3, finish: Vector3): Path
         frontWave, nextWave = nextWave, frontWave
         table.clear(nextWave)
     end
+    return true
+end
 
-    --If the finish node wasn't visited, no path can exist
-    if finishNode[IDX_TOTALCOST] == math.huge then
+
+--Update the costs so paths can be found from anywhere to finish
+function PathLib.UpdateMeshCosts(mesh: PathMesh, finish: Vector3): boolean
+    local finishNodePos = ToPathGridRound(finish)
+    local finishNode = mesh.Nodes[CoordToIndex(finishNodePos.X, finishNodePos.Z)]
+    
+    if not ValidateNode("Finish", finish, finishNode) then
+        return false
+    end
+    
+    return UpdateMeshCosts(mesh, finishNode)
+end
+
+
+--Find a path between the given nodes using the already-computed costs
+local function FindPathUsingCosts(mesh: PathMesh, startNode: PathNode, finishNode: PathNode): PathList?
+    --If the finish node wasn't the origin, the costs can't be used for this path
+    if finishNode[IDX_TOTALCOST] ~= 0 then
+        return nil
+    end
+    
+    --If the start node wasn't visited, no path can exist
+    if startNode[IDX_TOTALCOST] == math.huge then
         return nil
     end
 
-    --Follow the cheapest nodes from finish to start
-    local path: {PathNode} = {finishNode}
-    while path[#path] ~= startNode do
+    --Follow the cheapest nodes from start finish
+    local path: {PathNode} = {startNode}
+    while path[#path] ~= finishNode do
         local current = path[#path]
         local bestNeighbor: PathNode? = nil
 
         for _, neighbor in current[IDX_NEIGHBORS] do
-            if (not neighbor[IDX_BLOCKED]) and ((bestNeighbor == nil) or (neighbor[IDX_TOTALCOST] < bestNeighbor[IDX_TOTALCOST])) then
+            if ((bestNeighbor == nil) or (neighbor[IDX_TOTALCOST] < bestNeighbor[IDX_TOTALCOST])) then
                 bestNeighbor = neighbor
             end
         end
 
         --If a valid neighbor isn't found, no reason to keep searching
-        if (bestNeighbor == nil) or table.find(path, bestNeighbor) then
+        if (bestNeighbor == nil) then
             break
         end
         --Record the neighbor
@@ -289,19 +318,71 @@ function PathLib.FindPath(mesh: PathMesh, start: Vector3, finish: Vector3): Path
     end
     local pathLen = #path
 
-    --If the path didn't connect to the start node, a path doesn't exist
-    if path[pathLen] ~= startNode then
+    --If the path didn't connect to the finish node, a path doesn't exist
+    if path[pathLen] ~= finishNode then
         return nil
     end
 
     --Store the position of each node used in the correct (reversed, so start to finish) order
     local result: PathList = table.create(pathLen)
 
-    for i = 1, pathLen do
-        result[i] = path[pathLen - i + 1][IDX_POSITION]
+    for i, node in path do
+        result[i] = node[IDX_POSITION]
     end
 
     return result
 end
+
+
+--Find a path between the given locations using the already-computed costs
+function PathLib.FindPathUsingCosts(mesh: PathMesh, start: Vector3, finish: Vector3): PathList?
+    --Look up the start and finish locations in the node grid
+    local startNodePos = ToPathGridRound(start)
+    local finishNodePos = ToPathGridRound(finish)
+
+    local startNode = mesh.Nodes[CoordToIndex(startNodePos.X, startNodePos.Z)]
+    local finishNode = mesh.Nodes[CoordToIndex(finishNodePos.X, finishNodePos.Z)]
+
+    if not ValidateNode("Start", start, startNode) then
+        return nil
+    end
+
+    if not ValidateNode("Finish", finish, finishNode) then
+        return nil
+    end
+
+    if finishNode[IDX_TOTALCOST] ~= 0 then
+        warn("Finish node at (", finish, ") was not the finish used to update costs!")
+        return nil
+    end
+
+    return FindPathUsingCosts(mesh, startNode, finishNode)
+end
+
+
+--Find a path between the given locations using the path mesh. Updates costs
+function PathLib.FindPath(mesh: PathMesh, start: Vector3, finish: Vector3): PathList?
+    --Look up the start and finish locations in the node grid
+    local startNodePos = ToPathGridRound(start)
+    local finishNodePos = ToPathGridRound(finish)
+
+    local startNode = mesh.Nodes[CoordToIndex(startNodePos.X, startNodePos.Z)]
+    local finishNode = mesh.Nodes[CoordToIndex(finishNodePos.X, finishNodePos.Z)]
+
+    if not ValidateNode("Start", start, startNode) then
+        return nil
+    end
+
+    if not ValidateNode("Finish", finish, finishNode) then
+        return nil
+    end
+    
+    if not UpdateMeshCosts(mesh, finishNode) then
+        return nil
+    end
+    
+    return FindPathUsingCosts(mesh, startNode, finishNode)
+end
+
 
 return table.freeze(PathLib)
